306fe909dbd696ec14210197cfea53c0d7c88563
juan <jgdmjgdm@hotmail.com>
2020-06-08 20:02:09 -0500

    admi-coop fin

--- buscar-viaje/java.js
+++ buscar-viaje/java.js
@@ -10,31 +10,18 @@ busqueda.addEventListener('submit', function (e) {
     e.preventDefault();
 
     var valor_a_buscar = document.getElementById("txt_buscar").value;
+    console.log(valor_a_buscar);
     if (valor_a_buscar != "") {
         //document.getElementById("demo").innerHTML = "Buscando..."; //colocar valores en una etiqueta
-        buscar_campo.innerHTML = `<p class="temporal alerta_busqueda">Buscando...</p>`;
+        buscar_campo.innerHTML = `<p class="temporal">Buscando...</p>`;
         document.getElementById("demo").style.color = "blue";
         setTimeout(function () { document.querySelector('.temporal').remove(); }, 2000);
     } else {
         alerta_campo.innerHTML = `<div class="alert alert-info" role="alert">
-        <strong>Ingresa el nombre de una cooperativa</strong></div>`;
+        <strong>Ingresa el nombre de una coopertativa</strong></div>`;
         setTimeout(function () { document.querySelector('.alert').remove(); }, 3000);
     }
 })
 
-/*
-$(document).ready(function () {
-    $('#txt_buscar').keyup(function () {
-        _this = this;
-        // Show only matching TR, hide rest of them
-        //let tabla = document.getElementById("tabla_coop_public");
-        $.each($('#tabla_coop_public tbody tr'), function () {
-            if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1) {
-                $(this).hide();
-            } else {
-                $(this).show();
-            }
-        });
-    });
-});
-*/
+
+
