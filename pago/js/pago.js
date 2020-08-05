1b4e87743089a2c8d558420f095d97f5f5b4e213
joseleonec <joleonca@gmail.com>
2020-08-05 01:16:26 -0500

    Cargando datos para pagos

--- pago/js/pago.js
+++ pago/js/pago.js
@@ -7,29 +7,39 @@ var idUsuario;
 var idCarrito;
 var email;
 var usuario;
+
 emailAuth.onAuthStateChanged(user => {
     // USUARIO_AUTH = user;
-    if (user) {
-        email = user.email;
-        fetch(userURL + user.email.toString()).then(function (response) {
-            return response.json();
-        }).then(function (x) {
-            usuario = x;
-        }).catch(function () {
-            console.log("Error al hallar el ID de usuario");
-        });
-    } else {
-        window.location.href = "login.html";
-        console.log("compras no hay usuario");
-    }
+    // console.log("Verificando sesion");
+    // if (user) {
+    //     var userURL = 'https://terminal25backend.herokuapp.com/usuario/email=';
+    //     email = user.email;
+    //     console.log(email.toString());
+    //     fetch(userURL + user.email.toString()).then(function (response) {
+    //         return response.json();
+    //     }).then(function (x) {
+    //         usuario = x;
+    //     }).catch(function () {
+    //         console.log("Error al hallar el usuario");
+    //     });
+    //     fetch('https://terminal25backend.herokuapp.com/carrito/idusuario=' + usuario.idUsuario + '/estado=PENDIENTE').then(function (response) {
+    //         return response.json();
+    //     }).then(function (cart) {
+    //         idCarrito = cart.idCarrito;
+    //     }).catch(function () {
+    //         console.log("Error al hallar el ID de usuario");
+    //     });
+    // } else {
+    //     window.location.href = "login.html";
+    //     console.log("compras no hay usuario");
+    // }
 });
 
 function addRow(datatable, carrito) {
     datatable.row.add([carrito.idCarrito, carrito.fechaCreacion, carrito.estado]).draw();
 }
+
 // LLENAR TABLA
-
-
 function llenarTabla(datatable) {
     fetch(urlCarrito + "idusuario=" + idUsuario).then(function (response) {
         return response.json();
@@ -50,39 +60,73 @@ $(document).ready(function () {
         "info": false
     });
     cargarDetalle(datatableDetalle);
-    // generarCodigo(datatableDetalle);
+    // generarCodigo();
 });
 
 function cargarDetalle(datatableDetalle) {
     var subtotal = 0;
     var total = 0;
-    fetch(urlBoletos + "idcarrito=" + idCarrito).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        console.log(data);
-        data.forEach(boleto => {
-            console.log(boleto);
-            idCarrito = boleto.carrito.idCarrito;
-            datatableDetalle.row.add(
-                [
-                    boleto.itinerario.viaje.origen,
-                    boleto.itinerario.viaje.destino.nombre,
-                    boleto.cantidadDeAsientos,
-                    boleto.costo
-                ]
-            ).draw();
-            subtotal += boleto.costo;
-            subtotal = parseInt(subtotal * 100) / 100;
-        });
+    emailAuth.onAuthStateChanged(user => {
+        // USUARIO_AUTH = user;
+        console.log("Verificando sesion");
+        if (user) {
+            var userURL = 'https://terminal25backend.herokuapp.com/usuario/email=';
+            email = user.email;
+            console.log(email.toString());
+            fetch(userURL + user.email.toString()).then(function (response) {
+                return response.json();
+            }).then(function (x) {
+                console.log(x);
+                usuario = x;
+                idUsuario = x.idUsuario;
+                console.log(usuario);
+                fetch('https://terminal25backend.herokuapp.com/carrito/idusuario=' + usuario.idUsuario + '/estado=PENDIENTE').then(function (response) {
+                    return response.json();
+                }).then(function (cart) {
+                    idCarrito = cart[0].idCarrito;
+                    console.log(cart);
+                    fetch(urlBoletos + "idcarrito=" + idCarrito).then(function (response) {
+                        return response.json();
+                    }).then(function (data) {
+                        console.log(data);
+                        data.forEach(boleto => {
+                            console.log(boleto);
+                            idCarrito = boleto.carrito.idCarrito;
+                            datatableDetalle.row.add(
+                                [
+                                    boleto.itinerario.viaje.origen,
+                                    boleto.itinerario.viaje.destino.nombre,
+                                    boleto.cantidadDeAsientos,
+                                    boleto.costo
+                                ]
+                            ).draw();
+                            subtotal += boleto.costo;
+                            subtotal = parseInt(subtotal * 100) / 100;
+                        });
 
-        total = subtotal * 1.12;
-        total = parseInt(total * 100) / 100;
-        // total = total.toString(); //If it's not already a String
-        // total = total.slice(0, (total.indexOf(".")) + 3); //With 3 exposing the hundredths place
-        // Number(total); //If you need it back as a Number
-        document.querySelector('#pago-subtotal').innerText = subtotal;
-        document.querySelector('#pago-total').innerText = total;
+                        total = subtotal * 1.12;
+                        total = parseInt(total * 100) / 100;
+                        // total = total.toString(); //If it's not already a String
+                        // total = total.slice(0, (total.indexOf(".")) + 3); //With 3 exposing the hundredths place
+                        // Number(total); //If you need it back as a Number
+                        document.querySelector('#pago-subtotal').innerText = subtotal;
+                        document.querySelector('#pago-total').innerText = total;
+                    });
+                }).catch(function () {
+                    console.log("Error al hallar el ID de usuario");
+                });
+
+            }).catch(function () {
+                console.log("Error al hallar el usuario");
+            });
+
+        } else {
+            window.location.href = "login.html";
+            console.log("compras no hay usuario");
+        }
     });
+
+
 }
 
 // BARRA DE PROGRESO
