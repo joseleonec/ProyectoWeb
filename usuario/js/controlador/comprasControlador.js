bef6d7aee573f1d45679b3fdb305443c1e13bcbc
joseleonec <joleonca@gmail.com>
2020-08-05 21:31:26 -0500

    Descarga de la factura

--- usuario/js/controlador/comprasControlador.js
+++ usuario/js/controlador/comprasControlador.js
@@ -1,5 +1,8 @@
 const url = 'https://terminal25backend.herokuapp.com/factura/';
 const urlCounter = 'https://terminal25backend.herokuapp.com/boleto/count/'
+const urlCarritos = 'https://terminal25backend.herokuapp.com/carrito/idusuario=';
+const urlBoletos = 'https://terminal25backend.herokuapp.com/boleto/';
+const urlItinerario = 'https://terminal25backend.herokuapp.com/itinerario/';
 var idUsuario;
 
 function addRow(datatable, factura) {
@@ -34,37 +37,29 @@ function llenarTabla(datatable) {
 
 $(document).ready(function () {
 
-    // var datatable = $('#tablaCompras').DataTable({
-    //     "sDom": 'lrtip',
-    //     "searching": false,
-    //     "paging": false,
-    //     "info": false,
-    //     "columnDefs": [{
-    //         "targets": -1,
-    //         "data": null,
-    //         "defaultContent": `
-    //                 <button name="delete" class="btn btn-sm btn-danger">
-    //                     <i class="fa fa-trash"></i>
-    //                 </button>
-    //             `
-    //     }]
-    // });
     var datatable = $('#tablaCompras').DataTable({
         "info": false,
         "columnDefs": [{
             "targets": -1,
             "data": null,
             "defaultContent": `
-                    <button name="pdf" class="btn btn-sm btn-danger">
-                        Descargar PDF
-                    </button>
-                <button name="qr" class="btn btn-sm btn-secondary">
+            <button name="edit" type="submit" class="btn btn-info btn-rounded btn-sm my-0" 
+            data-toggle="modal" data-target="#exampleModal">
+                Detalle
+            </button>
+                <button name="qr" class="btn btn-sm btn-secondary qr">
                     Descargar QR
                 </button>
             `
         }]
     });
-
+    var datatableBoletos = $('#tablaBoletos').DataTable({
+        "sDom": 'lrtip',
+        "bSort": false,
+        "searching": false,
+        "paging": false,
+        "info": false
+    });
     // LLENAR TABLA
     emailAuth.onAuthStateChanged(user => {
         // USUARIO_AUTH = user;
@@ -118,8 +113,52 @@ $(document).ready(function () {
             mostrarMensaje('Elemento regisrado con exito', 'success');
         }
     });
+    // Reset modal form after close
     $('#exampleModal').on('hidden.bs.modal', function () {
-        $(this).find('form')[0].reset();
+        //  $(this).find('form')[0].reset();
+        datatableBoletos.clear().draw();
+    });
+
+    //modal show
+    $('#exampleModal').on('show.bs.modal', function (event) {
+        const datos = event.relatedTarget.parentElement.parentElement.getElementsByTagName('td');
+        // console.log(datos[0]);
+        const idCarrito = datos[2].innerText;
+        const estado = datos[2].innerText;
+        var modal = $(this)
+        modal.find('.modal-title').text('Carrito');
+        var title = document.querySelector("#exampleModalLabel");
+        title.name = idCarrito;
+        title.innerText += " " + title.name;
+        if (estado === "CADUCADO" || estado === "FINALIZADO") {
+            document.querySelector("#finalizar").disabled = true;
+            document.querySelector("#agregar-boletos").disabled = true;
+        }
+        var total = 0;
+        var subtotal = 0;
+        fetch(urlBoletos + "idcarrito=" + idCarrito).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data.status);
+            data.forEach(boleto => {
+                console.log(boleto.costo);
+                datatableBoletos.row.add(
+                    [
+                        boleto.idBoleto,
+                        boleto.itinerario.viaje.origen,
+                        boleto.itinerario.viaje.destino.nombre,
+                        boleto.cantidadDeAsientos,
+                        boleto.costo
+                    ]
+                ).draw();
+                subtotal += boleto.costo;
+                subtotal = parseInt(subtotal * 100) / 100;
+            });
+            total = subtotal * 1.12;
+            total = parseInt(total * 100) / 100;
+            document.querySelector('#pago-subtotal').innerText = subtotal;
+            document.querySelector('#pago-total').innerText = total;
+        });
     });
     // DELETE
     datatable.on('click', 'tbody tr', function (e) {
@@ -128,25 +167,29 @@ $(document).ready(function () {
         const ID = columns[0].innerText;
         if (botonname === 'pdf') {
             console.log(this);
+            // mostrarMensaje('Elemento eliminado ', 'info');
+            var form = $('.div'),
+                cache_width = form.width(),
+                a4 = [595.28, 841.89]; // for a4 size paper width and height  
+
+            console.log(this);
+            console.log(this);
+            $('body').scrollTop(0);
+            console.log("sssssssss");
+            createPDF();
             mostrarMensaje('Elemento eliminado ', 'info');
+
         } else if (botonname === 'qr') {
             let urlQr = "https://prebaflaskqr589.herokuapp.com/boleto/";
-            // fetch(urlQr + ID + ".png").then(function (response) {
-            //     return response.text();
-            // }).then(function (data) {
-            //     console.log("Codigo obtenido: " + data);
-            // }).catch(err => {
-            //     console.log("Error al obtener: " + err);
-            // });
             fetch(urlQr + "1" + ".png")
-            .then(function (resp) {
-                return resp.blob();
-            }).then(function (blob) {
-                download(blob);
-            });
-            console.log(urlQr + "1" + ".png");
-
-
+                .then(res => res.blob())
+                .then(blob => {
+                    var file = window.URL.createObjectURL(blob);
+                    // window.location.assign(file);
+                    // document.execCommand('SaveAs','true',file);
+                    window.open(file);
+                    // window.open("https://prebaflaskqr589.herokuapp.com/boleto/1.png",'_blank');
+                });
             console.log(this);
             // console.log(datos[1]);
             document.getElementById("labelid").readOnly = true;
@@ -155,4 +198,112 @@ $(document).ready(function () {
             // Eliminamos el registro que indica que la tabla esta vacia
         }
     });
+    var botonGetPDF = document.querySelector('#getPDF');
+    botonGetPDF.addEventListener('click', () => {
+        console.log(this);
+        const element = document.getElementById("detalleFactura");
+        // Choose the element and save the PDF for our user.
+        html2pdf()
+        .set({ html2canvas: { scale: 4 } })
+        .from(element)
+        .save();
+
+        console.log(this);
+        console.log(this);
+        $('body').scrollTop(0);
+        console.log("sssssssss");
+        createPDF();
+        mostrarMensaje('Elemento eliminado ', 'info');
+    });
 });
+
+// var
+//     form = $('#tablaBoletos'),
+//     cache_width = form.width(),
+//     a4 = [595.28, 841.89]; // for a4 size paper width and height  
+
+function createPDF() {
+    getCanvas().then(function (canvas) {
+        var
+            img = canvas.toDataURL("image/png"),
+            doc = new jsPDF({
+                unit: 'px',
+                format: 'a4'
+            });
+        doc.addImage(img, 'JPEG', 50, 50);
+        // doc.save('Bhavdip-html-to-pdf.pdf');
+        form.width(cache_width);
+    });
+}
+
+// create canvas object  
+function getCanvas() {
+    form.width((a4[0] * 1.33333) - 80).css('max-width', 'none');
+    return html2canvas(form, {
+        imageTimeout: 2000,
+        removeContainer: true
+    });
+}
+
+/* 
+* jQuery helper plugin for examples and tests 
+*/
+(function ($) {
+    $.fn.html2canvas = function (options) {
+        var date = new Date(),
+            $message = null,
+            timeoutTimer = false,
+            timer = date.getTime();
+        html2canvas.logging = options && options.logging;
+        html2canvas.Preload(this[0], $.extend({
+            complete: function (images) {
+                var queue = html2canvas.Parse(this[0], images, options),
+                    $canvas = $(html2canvas.Renderer(queue, options)),
+                    finishTime = new Date();
+
+                $canvas.css({ position: 'absolute', left: 0, top: 0 }).appendTo(document.body);
+                $canvas.siblings().toggle();
+
+                $(window).click(function () {
+                    if (!$canvas.is(':visible')) {
+                        $canvas.toggle().siblings().toggle();
+                        throwMessage("Canvas Render visible");
+                    } else {
+                        $canvas.siblings().toggle();
+                        $canvas.toggle();
+                        throwMessage("Canvas Render hidden");
+                    }
+                });
+                throwMessage('Screenshot created in ' + ((finishTime.getTime() - timer) / 1000) + " seconds<br />", 4000);
+            }
+        }, options));
+
+        function throwMessage(msg, duration) {
+            window.clearTimeout(timeoutTimer);
+            timeoutTimer = window.setTimeout(function () {
+                $message.fadeOut(function () {
+                    $message.remove();
+                });
+            }, duration || 2000);
+            if ($message)
+                $message.remove();
+            $message = $('<div ></div>').html(msg).css({
+                margin: 0,
+                padding: 10,
+                background: "#000",
+                opacity: 0.7,
+                position: "fixed",
+                top: 10,
+                right: 10,
+                fontFamily: 'Tahoma',
+                color: '#fff',
+                fontSize: 12,
+                borderRadius: 12,
+                width: 'auto',
+                height: 'auto',
+                textAlign: 'center',
+                textDecoration: 'none'
+            }).hide().fadeIn().appendTo('body');
+        }
+    };
+})(jQuery);
