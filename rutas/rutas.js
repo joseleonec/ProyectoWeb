df2630aa52ea017129a25fc29ad8374c5083b401
joseleonec <joleonca@gmail.com>
2020-08-06 13:46:39 -0500

    Corregido rutas

--- rutas/rutas.js
+++ rutas/rutas.js
@@ -89,67 +89,107 @@ $(document).on('click', '.verMapa', function (e) {
     graficarMapa(latitudeOrigen, longitudeOrigen, latitudeDestino, longitudeDestino);
 });
 
+var counter = 0;
 var mymap = null;
+var id, target, option;
 
+function success(pos) {
+    var crd = pos.coords;
+
+    if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+        console.log('Congratulation, you reach the target');
+        navigator.geolocation.clearWatch(id);
+    }
+};
+
+function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+target = {
+    latitude: 0,
+    longitude: 0,
+}
+
+options = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+function updateMapa() {
+    $("#mapa").load(window.location.href + " #mapa");
+}
+var control;
 function graficarMapa(latitudeOrigen, longitudeOrigen, latitudeDestino, longitudeDestino) {
+
     if (navigator.geolocation) {
         console.log("Graficando");
         navigator.geolocation.getCurrentPosition(function (position) {
-            //var latitude = position.coords.latitude;
-            //var longitude = position.coords.longitude;
-
-            /*  mymap = L.map('mapa', {
-                 center: [latitudeDestino, longitudeDestino],
-                 zoom: 12
-             }); */
-
-            /*L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-                maxZoom: 25,
-                attribution: 'Datos del mapa de &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imágenes © <a href="https://www.mapbox.com/">Mapbox</a>',
-                id: 'mapbox/streets-v11'
-            }).addTo(mymap);*/
             console.log(mymap);
-            L.Routing.control({
+            if (counter != 0) {
+                // var rlayer = L.Routing.control({
+                //     waypoints: [
+                //         L.latLng(s[0], s[1]),
+                //         L.latLng(e[0], e[1])
+                //     ]
+                // });
+                // routesLayer.clearLayers();
+                // routesLayer.addLayer(rlayer);
+                control.getPlan().setWaypoints([]);
+            }
+            control = L.Routing.control({
                 waypoints: [
                     L.latLng(latitudeDestino, longitudeDestino),
                     L.latLng(latitudeOrigen, longitudeOrigen)
                 ],
                 language: 'es'
             }).addTo(mymap);
+            counter += 1;
+            // mymap.removeControl(control);
+            control.hide();
+
+
+            // control.show();
         });
     }
 }
+iniciarMapa();
 
-if (navigator.geolocation) {
-    console.log("Mapa inicializado");
-    navigator.geolocation.getCurrentPosition(function (position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
+function iniciarMapa() {
+    if (navigator.geolocation) {
+        console.log("Mapa inicializado");
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
 
+            mymap = L.map('mapa', {
+                center: [latitude, longitude],
+                zoom: 8
+            });
+
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 15,
+                attribution: 'Datos del mapa de &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imágenes © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox/streets-v11'
+            }).addTo(mymap);
+
+        });
+    } else {
         mymap = L.map('mapa', {
-            center: [latitude, longitude],
-            zoom: 12
+            center: [-2.7397435, -78.86261],
+            zoom: 17
         });
 
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            maxZoom: 25,
+            maxZoom: 15,
             attribution: 'Datos del mapa de &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imágenes © <a href="https://www.mapbox.com/">Mapbox</a>',
             id: 'mapbox/streets-v11'
         }).addTo(mymap);
 
-    });
-} else {
-    mymap = L.map('mapa', {
-        center: [-2.7397435, -78.86261],
-        zoom: 17
-    });
-
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 25,
-        attribution: 'Datos del mapa de &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imágenes © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/streets-v11'
-    }).addTo(mymap);
+    }
 }
+
 
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({ pageLanguage: 'es', includedLanguages: 'es,sp,ca,eu,gl,en,fr,it,pt,de', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true }, 'google_translate_element');
